efe8a06d36d4f550a5ae8e502bde68be9b3067be
swapnil-deotare <swapnildeotare@gmail.com>
2023-08-07 17:57:10 -0700

    items api

--- Repositories/InMemItemsRepository.cs
+++ Repositories/InMemItemsRepository.cs
@@ -11,6 +11,15 @@ namespace Catalog.Repositories
             new Item {Id = Guid.NewGuid(), Name = "Iron Sword" , Price = 20, CreatedDate = DateTimeOffset.UtcNow},
             new Item {Id = Guid.NewGuid(), Name = "Bronze Shield" , Price = 18, CreatedDate = DateTimeOffset.UtcNow},
         };
+        public IEnumerable<Item> GetItems()
+        {
+            return items;
+        }
+
+        public Item GetItem(Guid id)
+        {
+            return items.SingleOrDefault(item => item.Id == id);
+        }
     }
 
 
